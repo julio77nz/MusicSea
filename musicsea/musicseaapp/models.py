30389493b1c00742b5d1007b56e2e509b9cd114e
julio77nz <julioqr94@gmail.com>
2017-04-19 06:03:46 +0200

    Initial model done.

--- musicsea/musicseaapp/models.py
+++ musicsea/musicseaapp/models.py
@@ -1,3 +1,51 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+class Group(models.Model):
+    name = models.TextField()
+    genre = models.TextField()
+    birthdate = models.DateField()
+    bibliography = models.DateField()
+    image = models.TextField()
+    user = models.ForeignKey(User, default=1)
+
+    def __unicode__(self):
+        return u"%s" % self.name
+
+class Artist(models.Model):
+    name = models.TextField()
+    group = models.TextField()
+    gender = models.TextField()
+    birthdate = models.DateField()
+    bibliography = models.DateField()
+    image = models.TextField()
+    user = models.ForeignKey(User, default=1)
+    group = models.ForeignKey(Group, null=True, related_name='artists')
+
+    def __unicode__(self):
+        return u"%s" % self.name
+
+class Album(models.Model):
+    name = models.TextField()
+    group = models.TextField()
+    release = models.DateField()
+    bibliography = models.DateField()
+    image = models.TextField()
+    user = models.ForeignKey(User, default=1)
+    group = models.ForeignKey(Group, null=True, related_name='albums')
+
+    def __unicode__(self):
+        return u"%s" % self.name
+
+class Song(models.Model):
+    name = models.TextField()
+    group = models.TextField()
+    album = models.TextField()
+    lyrics = models.TextField()
+    bibliography = models.DateField()
+    image = models.TextField()
+    user = models.ForeignKey(User, default=1)
+    album = models.ForeignKey(Album, null=True, related_name='songs')
+
+    def __unicode__(self):
+        return u"%s" % self.name
